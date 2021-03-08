fb480f6063be87a4e2ab2644719110d87c4d29c7
Joon Yoon <myexistance@HOTMAIL.COM>
2021-03-09 11:46:07 +1300

    async/await analogy added

--- learn-javascript/notes/async/async.js
+++ learn-javascript/notes/async/async.js
@@ -49,3 +49,24 @@ function pickOnlyOne() {
 }
 
 pickOnlyOne().then(console.log);
+
+//Async/Await Analogy: A mrning routine to be done  before going to work at 8am
+
+// 1. Take a shower
+// 2. Eat Breakfast
+// 3. Get Dressed
+// 4. Put on Make up
+// 5. Check Email
+
+// doing then in a row or no matter the order.
+// you can't get dressed before completing the shower.
+
+const morningRoutine = async (startTime) => {
+  const step1FinishTime = await shower(startTime)
+  const step2FinishTime = await eatbreakfast(step1FinshTime)
+  const step3FinishTime = await getDressed(step2FinshTime)
+  const step4FinishTime = await putOnMakeup(step3FinshTime)
+  const step5FinishTime = await checkEmail(step4FinshTime)
+  return step5FinshTime
+}
+
